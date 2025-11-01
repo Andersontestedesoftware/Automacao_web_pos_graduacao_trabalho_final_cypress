8fb2731c2a279acdb9fc3ba901ce0ecc6f995006
Anderson Bonfim <48413472+Andersontestedesoftware@users.noreply.github.com>
2025-11-01 20:42:14 -0300

    remover arquivo não usado

--- cypress/pages/login/loginUser.js
+++
@@ -1,8 +0,0 @@
-import auth from '../account/auth.page';
-import account from '../account/account.page';
-
-// Helper para efetuar login e verificar que o usuário está logado
-export function loginUser(email = 'qa-tester-1759530219181@test.com', password = '12345', fullName = 'QA Tester') {
-  auth.login(email, password);
-  account.verifyLoggedIn(fullName);
-}
